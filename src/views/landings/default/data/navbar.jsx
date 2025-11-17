78f4c26d571654d0979527860cb4d78fc16a1785
NakamaShp <khlfaalhsn5@gmail.com>
2025-11-17 11:24:34 +0700

    fix(navbar): correct secondaryBtn typo and swap auth button priority

--- src/views/landings/default/data/navbar.jsx
+++ src/views/landings/default/data/navbar.jsx
@@ -1,27 +1,17 @@
 // @project
-import SvgIcon from '@/components/SvgIcon';
-import { PAGE_PATH, SOSIAL_MEDIA_URL, FREEBIES_URL, PRIVIEW_PATH } from '@/path';
+import { PAGE_PATH, SIGNIN_PATH, SIGNUP_PATH } from '@/path';
 
 /***************************  DEFAULT - NAVBAR  ***************************/
 
-// const linkProps = { target: '_blank', rel: 'noopener noreferrer' };
-
 export const navbar = {
   customization: true,
-  secondaryBtn: {
-    target: '_blank',
-    rel: 'noopener noreferrer',
-    children: <SvgIcon name="tabler-brand-facebook" color="primary.main" size={18} />,
-    children: <SvgIcon name="tabler-brand-instagram" color="primary.main" size={18} />,
-    link: SOSIAL_MEDIA_URL.instagram,
-    sx: { minWidth: 40, width: 40, height: 40, p: 0 }
-  },
-  primaryBtn: { children: 'Mulai Sekarang', link: PAGE_PATH.contact },
+  primaryBtn: { children: 'SignUp', link: SIGNUP_PATH },
+  secondaryBtn: { children: 'SignIn', link: SIGNIN_PATH },
   navItems: [
     { id: 'home', title: 'Home', link: '/' },
-    { id: 'about', title: 'Tentang Kami', link: PAGE_PATH.about },
-    { id: 'servicec', title: 'Layanan', link: PAGE_PATH.service },
-    { id: 'portfolio', title: 'Portfolio', link: PAGE_PATH.portfolio },
+    { id: 'about', title: 'About', link: PAGE_PATH.about },
+    { id: 'servicec', title: 'Service', link: PAGE_PATH.service },
+    { id: 'portfolio', title: 'Porfolio', link: PAGE_PATH.portfolio },
     { id: 'blog', title: 'Blog', link: PAGE_PATH.blog }
   ]
 };
